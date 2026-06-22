f14d7c3c0083f8e710af5860f09952c2a607f112
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2026-06-22 18:26:12 +0100

    feat: add hero image assets and update App component layout with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 function App() {
     return (
         <>
-            <h1>jpoc</h1>
+            <h1 className="text-3xl font-bold underline">
+                jpoc image optimization check{" "}
+            </h1>
+            <h2 className="text-2xl font-bold underline">Image one </h2>
+            <img src="/images/hero.svg" alt="hero-image" />
+
+            <h2 className="text-2xl font-bold underline">Image two </h2>
+            <img
+                src="https://res.cloudinary.com/dsrxjzmcl/image/upload/v1782149133/hero_qinia7.png"
+                alt="hero-image"
+            />
+
+            {/* <h2>Image three </h2>
+            <img src="/images/hero.svg" alt="hero-image" /> */}
         </>
     );
 }
